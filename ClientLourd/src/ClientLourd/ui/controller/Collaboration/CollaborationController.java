e10fbc45904b8b35dc180de0a4d506ed660f45ef
Drydge <antho.deveaux@gmail.com>
2016-04-28 16:41:31 +0200

    Client lourds
    collabo controller bouton supprimer et modif

--- ClientLourd/src/ClientLourd/ui/controller/Collaboration/CollaborationController.java
+++ ClientLourd/src/ClientLourd/ui/controller/Collaboration/CollaborationController.java
@@ -104,13 +104,69 @@ public class CollaborationController extends Ressource{
         supprimerCollaborateurButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (colList.isSelectionEmpty()){
+                    JOptionPane.showMessageDialog(col,
+                            "aucun collaborateur selectionné",
+                            "error: ",
+                            JOptionPane.ERROR_MESSAGE);
+                }else{
+                    try {
+                        Socket client = new Socket(SERVER_ADRESS,PORT_UPDATE);
+                        ArrayList<String> insert= new ArrayList<String>();
+                        insert.add("collab");insert.add("remove");insert.add(pseudo);insert.add(type);insert.add(id);insert.add((String)colList.getSelectedValue());
+                        if(adminCheckBox.isSelected())insert.add("1");
+                        else insert.add("0");
+                        if(ecritureCheckBox.isSelected())insert.add("1");
+                        else insert.add("0");
+                        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+                        oos.writeObject(insert);
+                        oos.flush();
+                        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+                        ArrayList<String> result = (ArrayList<String>) ois.readObject();
+                        if (result.size()>0){
+                            JOptionPane.showMessageDialog(col,
+                                    result,
+                                    "error: ",
+                                    JOptionPane.ERROR_MESSAGE);
+                        }else evtManager.TriggerColListChangeEvent();
+                    } catch (IOException | ClassNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
             }
         });
         modicationDroitsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (colList.isSelectionEmpty()){
+                    JOptionPane.showMessageDialog(col,
+                            "aucun collaborateur selectionné",
+                            "error: ",
+                            JOptionPane.ERROR_MESSAGE);
+                }else{
+                    try {
+                        Socket client = new Socket(SERVER_ADRESS,PORT_UPDATE);
+                        ArrayList<String> insert= new ArrayList<String>();
+                        insert.add("collab");insert.add("modif");insert.add(pseudo);insert.add(type);insert.add(id);insert.add((String)colList.getSelectedValue());
+                        if(adminCheckBox.isSelected())insert.add("1");
+                        else insert.add("0");
+                        if(ecritureCheckBox.isSelected())insert.add("1");
+                        else insert.add("0");
+                        ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+                        oos.writeObject(insert);
+                        oos.flush();
+                        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+                        ArrayList<String> result = (ArrayList<String>) ois.readObject();
+                        if (result.size()>0){
+                            JOptionPane.showMessageDialog(col,
+                                    result,
+                                    "error: ",
+                                    JOptionPane.ERROR_MESSAGE);
+                        }else evtManager.TriggerColListChangeEvent();
+                    } catch (IOException | ClassNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
             }
         });
     }
